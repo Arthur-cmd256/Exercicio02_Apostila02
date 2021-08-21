eb55501b6f3d351bf1b475945160cf625b14233f
Arthur-cmd256 <arthur.nascimento2020@outlook.com>
2021-08-21 15:13:56 -0300

    Resolução do exercicio por meio do IF/ELSE

--- Ex2Ap2.java
+++ Ex2Ap2.java
@@ -1,7 +1,15 @@
+import javax.swing.JOptionPane;
+
 public class Ex2Ap2{
         public static void main(String[] args) {
             
-            
+            int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a sua idade: "));
+            if (idade >= 18){
+                JOptionPane.showMessageDialog(null, "Voce e maior de idade!");
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Voce nao e maior de idade ainda :-(");
+            }
 
         }
 }
