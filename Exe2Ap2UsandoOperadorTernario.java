54811f0ed96072917d88287a5f74d10d64878639
Arthur-cmd256 <arthur.nascimento2020@outlook.com>
2021-08-21 15:30:54 -0300

    Resolução do exercicio por meio do Operador Ternario

--- Exe2Ap2UsandoOperadorTernario.java
+++ Exe2Ap2UsandoOperadorTernario.java
@@ -1,8 +1,12 @@
+import javax.swing.JOptionPane;
+
 public class Exe2Ap2UsandoOperadorTernario {
 
     public static void main(String[] args) {
         
-        
+        int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a sua idade: "));
+
+        JOptionPane.showMessageDialog(null, String.format("Voce%se maior de idade", idade >= 18 ? " " : " nao "));
 
     }
     
